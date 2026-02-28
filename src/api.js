04b78a8872fe8b1f6cca0216d425126948ecf0c9
incomelogins-commits <incomelogins@gmail.com>
2026-02-28 23:52:31 +0000

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const BASE_URL = 'https://cryptomine-backend-production.up.railway.app';
 
 const api = axios.create({ baseURL: BASE_URL });
 
